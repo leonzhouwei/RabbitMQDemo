f7e7e6763240b296809630320b32d7df6e1ba731
Leon Zhou <leonzhouwei@gmail.com>
2012-07-24 20:41:35 +0800

    2012-07-24 20:41

---
+++ src/AGearIosBuilder.java
@@ -0,0 +1,34 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class AGearIosBuilder {
+	public static void main(String[] argv) throws Exception {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(AGearConfig.QUEUE_SERVER_IP);
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.exchangeDeclare(AGearConfig.EXCHANGE_NAME, "direct");
+		String bindKey = AGearConfig.EXCHANGE_QUEUE_BIND_KEY_IOS;
+		channel.queueDeclare(AGearConfig.QUEUE_NAME_IOS, false, false, false, null);
+		channel.queueBind(AGearConfig.QUEUE_NAME_IOS, AGearConfig.EXCHANGE_NAME, bindKey);
+
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(AGearConfig.QUEUE_NAME_IOS, true, consumer);
+
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			String routingKey = delivery.getEnvelope().getRoutingKey();
+
+			System.out.println(" [x] Received '" + routingKey + "':'" + message
+					+ "'");
+		}
+	}
+
+}
---
+++ src/AGearSender.java
@@ -0,0 +1,46 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class AGearSender {
+	public static void main(String[] argv) throws Exception {
+		
+//		if (argv == null || argv.length < 1) {
+//			System.err.println("argv cannot be null or empty(AGearSender.java:9)");
+//			System.exit(1);
+//		}
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(AGearConfig.QUEUE_SERVER_IP);
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.exchangeDeclare(AGearConfig.EXCHANGE_NAME, "direct");
+
+//		String bindKey = getBindKey(argv);
+		String bindKey = AGearConfig.EXCHANGE_QUEUE_BIND_KEY_IOS;
+//		String message = getMessage(argv, AGearConfig.QUEUE_MSG_DELIMITER);
+		String message = "AGearSender";
+		
+		channel.basicPublish(AGearConfig.EXCHANGE_NAME, bindKey, null,
+				message.getBytes());
+		System.out.println(" [x] Sent '" + bindKey + "':'" + message + "'");
+
+		channel.close();
+		connection.close();
+	}
+
+	private static String getBindKey(String[] strings) {
+		return strings[0];
+	}
+
+	private static String getMessage(String[] strings, String delimiter) {
+		int length = strings.length;
+		StringBuilder words = new StringBuilder(strings[1]);
+		for (int i = 2; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
+	}
+
+}
